b112ea27e689a2509dd9398cad864d7552bd72f5
AlxndrSmk <voprosy262@gmail.com>
2023-03-21 17:18:29 +0300

    fix: handled the absence of an argument

--- src/what-season.js
+++ src/what-season.js
@@ -11,19 +11,16 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
- function getSeason(date) {
+function getSeason(date) {
   let seasons = ['winter', 'spring', 'summer', 'autumn'];
+  if (!date) return 'Unable to determine the time of year!' ;
+
   let m = date.getMonth() + 1;
 
-  if (m > 2 & m < 6) {
-      return seasons[1];
-  } else if (m > 5 & m < 9) {
-      return seasons[2];
-  } else if (m > 8 & m < 12) {
-      return seasons[3];
-  } else {
-      return seasons[0];
-  }
+  if (m > 2 & m < 6) return seasons[1];
+  else if (m > 5 & m < 9) return seasons[2];
+  else if (m > 8 & m < 12) return seasons[3];
+  else return seasons[0];
 }
 
 module.exports = {
