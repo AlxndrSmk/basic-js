626aaabeba92ccf1a40576c308843cd90d09b52c
AlxndrSmk <voprosy262@gmail.com>
2023-03-22 13:37:21 +0300

    feat: add next tests solution

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -14,6 +14,11 @@ const chainMaker = {
     return this;
   },
   removeLink(position) {
+    if (position <= 0 || position > this.chain.length || typeof position !== 'number') {
+      this.chain = [];
+      throw Error("You can\'t remove incorrect link!");
+    }
+
     this.chain.splice(position - 1, 1);
     return this;
   },
