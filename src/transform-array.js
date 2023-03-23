970f593796e22a521e04072099aeaaf747620280
AlxndrSmk <voprosy262@gmail.com>
2023-03-23 17:18:44 +0300

    feat: add isArray check

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,12 +13,14 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
- function transform(arr) {
+function transform(arr) {
+  if (!Array.isArray(arr)) throw Error("\'arr\' parameter must be an instance of the Array!");
+
   for (let i = 0; i < arr.length; i++) {
-      if (arr[i + 1] == '--discard-prev') arr.splice(arr[i - 1], 2);
-      if (arr[i + 1] == '--discard-next') arr.splice(arr[i], 2);
-      if (arr[i] == '--double-next') arr[i] = arr[i + 1];
-      if (arr[i] == '--double-prev') arr[i] = arr[i - 1];
+    if (arr[i + 1] == '--discard-prev') arr.splice(arr[i - 1], 2);
+    if (arr[i + 1] == '--discard-next') arr.splice(arr[i], 2);
+    if (arr[i] == '--double-next') arr[i] = arr[i + 1];
+    if (arr[i] == '--double-prev') arr[i] = arr[i - 1];
   }
   return arr;
 }
