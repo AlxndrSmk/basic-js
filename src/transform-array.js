1c169b0c6f30e88bf3629a947b2e79210ed12a3f
AlxndrSmk <voprosy262@gmail.com>
2023-03-24 15:25:12 +0300

    feat: add final solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -15,14 +15,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  */
 function transform(arr) {
   if (!Array.isArray(arr)) throw Error("\'arr\' parameter must be an instance of the Array!");
+  let res = [];
 
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i + 1] == '--discard-prev') arr.splice(arr[i - 1], 2);
-    if (arr[i + 1] == '--discard-next') arr.splice(arr[i], 2);
-    if (arr[i] == '--double-next') arr[i] = arr[i + 1];
-    if (arr[i] == '--double-prev') arr[i] = arr[i - 1];
+  if (arr.length > 0) {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] === '--double-prev') res.push(res[i - 1]);
+      else if (arr[i] === '--double-next') res.push(arr[i + 1]);
+      else if (arr[i] === '--discard-prev') {
+        res[i - 1] = undefined;
+        res.push(undefined);
+      }
+      else if (arr[i] === '--discard-next') {
+        res.push(undefined);
+      }
+      else if (arr[i - 1] === '--discard-next') res.push(undefined);
+      else res.push(arr[i])
+    }
   }
-  return arr;
+
+  return res.filter(e => typeof e !== 'undefined');
 }
 
 module.exports = {
