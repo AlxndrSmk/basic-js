127842638fef94f609ff2c7a608cf86e9b40795c
Alexander Samak <voprosys262@gmail.com>
2023-04-15 01:17:57 +0300

    finally add base encryption

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -19,67 +19,36 @@ const { NotImplementedError } = require('../extensions/index.js');
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
  * 
  */
- class VigenereCipheringMachine {
+class VigenereCipheringMachine {
 
-  encrypt(mes, key, mode) {
-    if (!mes || !key) throw Error ("Incorrect arguments!")
-      key = key.toUpperCase(); // подгон ключа по высоте
-      mes = mes.toUpperCase(); // подгон сообщения по высоте
-      let alph = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";		// Строка алфавита
+  encrypt(mes, key) {
+    if (!mes || !key) throw Error("Incorrect arguments!");
+    key = key.toUpperCase().repeat(mes.length).slice(0, mes.length) //привожу шифр к длине строки
+    mes = mes.toUpperCase();
 
-      if (arguments.length < 2 || !arguments.length || Object.values(arguments).includes(undefined)) throw Error('Incorrect arguments!');
+    let alph = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let res = '';
 
-      let maxlength = Math.max(mes.length, key.length);
-      let res = '';
+    for (let i = 0, j = 0; i < mes.length; i++) {
+      let symb = mes[i];
 
-      key = key.repeat(3).slice(0, mes.length) //привожу шифр к длине строки
-
-      for (let i = 0; i < maxlength; i++) {
-          console.log(key, mes)
-
-          let mesIndex = alph.indexOf(mes[((i >= mes.length) ?
-              i % mes.length :
-              i)]);	// если сообщение короче ключа
-
-          let keyIndexChar = key[((i >= key.length) ?
-              i % key.length :
-              i)]; // если ключ короче сообщения
-
-          let keyIndex = (typeof mode !== 'undefined') ?
-              parseInt(keyIndexChar) :
-              alph.indexOf(keyIndexChar);
-          // вычитание при дешифровании, либо сложение.
-          // console.log('mesIndex - ', mesIndex, 'keyIndexChar - ', keyIndexChar, 'keyIndex - ', keyIndex)
-
-          let char = '';
-          if (mesIndex === -1) {
-              char = ' ';
-              mesIndex++;
-          } else {
-              char = alph[(((alph.length + (mesIndex + keyIndex)) % alph.length))];
-          }
-
-          // char = alph.split('').find((e) => e == mesIndex) || alph.split('').find((e) => e == keyIndex) ?
-          //     alph[(((alph.length + (mesIndex + keyIndex)) % alph.length))] :
-          //     char;
-
-          // if (mesIndex === -1) {
-          //     char = ' ';
-          //     console.log(mesIndex)
-          // } else {
-          //     char = char;
-          // }
-
-          res += char; // добавить символ к результату.
+      if (symb.charCodeAt() > 64 && symb.charCodeAt() < 91) {
+        symb = alph[(alph.length + alph.indexOf(mes[i]) + alph.indexOf(key[j])) % alph.length];
+        j++;
+      } else {
+        symb = mes[i];
       }
 
-      return res.slice(0, mes.length); // вернуть строку результата    
+      res += symb;
+    }
+
+    return res; // вернуть строку результата    
   }
   decrypt(mes, key) {
-    if (!mes || !key) throw Error ("Incorrect arguments!");
-
+    if (!mes || !key) throw Error("Incorrect arguments!");
   }
 }
+
 module.exports = {
   VigenereCipheringMachine
 };
